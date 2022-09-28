0315ad774bbe4bfdfdb59fe90dbd1c1c1a313aff
rbrignall <rbrignall@gmail.com>
2022-09-28 23:54:39 +0100

    Tweaking look and feel

--- src/components/ActionBar/Timer.svelte
+++ src/components/ActionBar/Timer.svelte
@@ -9,9 +9,9 @@
 	<button class="btn btn-round" on:click={() => $gamePaused ? resumeGame() : pauseGame()} title="{$gamePaused ? 'Resume Game' : 'Pause Game'}">
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			{#if $gamePaused}
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.19 9.61L9.01 3.48A2.87 2.87 0 004.54 5.88v12.25a2.87 2.87 0 004.47 2.39l9.18-6.12a2.87 2.87 0 000-4.78v0z" />
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M18.19 9.61L9.01 3.48A2.87 2.87 0 004.54 5.88v12.25a2.87 2.87 0 004.47 2.39l9.18-6.12a2.87 2.87 0 000-4.78v0z" />
 			{:else}
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 3v18M18 3v18" />
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M6 3v18M18 3v18" />
 			{/if}
 		</svg>
 	</button>
@@ -27,6 +27,6 @@
 	}
 
 	.timer-text {
-		@apply px-4 text-2xl;
+		@apply px-2 text-xl sm:text-2xl sm:px-4;
 	}
 </style>
